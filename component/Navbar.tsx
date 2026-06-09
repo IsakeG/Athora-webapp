fc603a877127d23f16d6fc8c32bc72f7ad709686
IsakeG <isakegrant@gmail.com>
2026-06-09 09:00:41 -0400

    Update Navbar.tsx

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <nav>
+    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
       <h2>Athora</h2>
-      <ul>
-        <li>News</li>
-        <li>Athletes</li>
-        <li>Rankings</li>
-        <li>Events</li>
-      </ul>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Link href="/">Home</Link>
+        <Link href="/news">News</Link>
+        <Link href="/athletes">Athletes</Link>
+        <Link href="/rankings">Rankings</Link>
+        <Link href="/events">Events</Link>
+      </div>
     </nav>
   );
 }
