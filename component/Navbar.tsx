44466b6483bc8e0804e8baade6f17ee552d261ac
IsakeG <isakegrant@gmail.com>
2026-06-09 08:45:12 -0400

    Create Navbar.tsx

---
+++ component/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Athora</h2>
+      <ul>
+        <li>News</li>
+        <li>Athletes</li>
+        <li>Rankings</li>
+        <li>Events</li>
+      </ul>
+    </nav>
+  );
+}
